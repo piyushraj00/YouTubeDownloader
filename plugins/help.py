bda06ab4fc744b5560bb991051435f23f6fedf6a
piyushraj00 <80516641+piyushraj00@users.noreply.github.com>
2021-07-17 14:30:37 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -1,10 +1,10 @@
 from pyrogram import Client, Filters
-from aiogram.utils.callback_data import CallbackData
-
-select_lang_cb = CallbackData("select_lang_cb", "help", "back_btn")
 
 
-@Client.on_message(Filters.command(["help"]))
+
+
+
+@Client.on_message(Filters.command(["download"]))
 async def start(client, message):
     helptxt = f"✏️Enter The YouTube Video Url To Download"
     await message.reply_text(helptxt)
